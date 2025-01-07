5fe92551b7d21ab6f29db05e2e5ccae9614930f9
Santu-Hansariafood <Santu@hansariafood.com>
2025-01-07 16:49:10 +0530

    Seller model is updated

--- models/Seller.js
+++ models/Seller.js
@@ -1,47 +1,26 @@
 const mongoose = require("mongoose");
 
-const SellerSchema = new mongoose.Schema(
+const sellerSchema = new mongoose.Schema(
   {
     sellerName: { type: String, required: true, maxlength: 50 },
-    password: { type: String, required: true, minlength: 4, maxlength: 25 },
-    phoneNumbers: [
-      {
-        value: { type: String, required: true, match: /^[0-9]{10,15}$/ },
-      },
-    ],
-    emails: [
-      {
-        value: {
-          type: String,
-          required: true,
-          match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-        },
-      },
-    ],
+    password: { type: String, required: true },
+    phoneNumbers: [{ value: { type: String, required: true } }],
+    emails: [{ value: { type: String, required: true } }],
     commodities: [
       {
         name: { type: String, required: true },
         brokerage: { type: Number, default: 0 },
       },
     ],
-    selectedCompany: [
-      {
-        value: { type: String, required: true },
-        label: { type: String, required: true },
-      },
-    ],
+    companies: [String],
     selectedStatus: {
       type: String,
       enum: ["active", "inactive"],
-      required: true,
+      default: "active",
     },
-    buyers: [
-      {
-        name: { type: String, required: true },
-      },
-    ],
+    buyers: [{ name: String }],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Seller", SellerSchema);
+module.exports = mongoose.model("Seller", sellerSchema);
