25824ceb59edc1cd11b6603b7be4fc3554f4752d
Santu-Hansariafood <Santu@hansariafood.com>
2026-02-27 13:53:00 +0530

    updated

--- index.js
+++ index.js
@@ -158,6 +158,31 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
+const allowedOrigins = [
+  "https://client-vert-rho.vercel.app",
+  "http://localhost:3000"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow non-browser requests
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(
+          new Error("CORS not allowed from this origin"),
+          false
+        );
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 const helmet = require("helmet");
 const compression = require("compression");
 const rateLimit = require("express-rate-limit");
